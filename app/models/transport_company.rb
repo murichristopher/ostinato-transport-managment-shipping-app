5ee0e300dfe3f6427a62d32902c86f7dd061d9a7
murichristopher <murichristopher@outlook.com>
2022-05-23 13:54:42 -0300

    Modelo de Fornecedora recebe validação

--- app/models/transport_company.rb
+++ app/models/transport_company.rb
@@ -1,6 +1,8 @@
 class TransportCompany < ApplicationRecord
   validates :trading_name, :company_name, :domain, :registration_number, :full_address, presence: true
-  validates :registration_number, uniqueness:true
+  validates :trading_name, :company_name, :domain, :registration_number, uniqueness:true
   validates :registration_number, format: { with: /\A[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2}\z/
   }
+  validates :domain, length: {maximum: 30}, format: { with: /\S+\S+\.\S+/ }
+
 end
--- spec/models/transport_company_spec.rb
+++ spec/models/transport_company_spec.rb
@@ -1,4 +1,121 @@
 require 'rails_helper'
 
 RSpec.describe TransportCompany, type: :model do
+  describe "#valid?" do
+    context "presence" do
+      it "false when 'trading_name' is empty" do
+        transport_company = TransportCompany.new(trading_name: '')
+        transport_company.valid?
+
+        res = transport_company.errors[:trading_name]
+
+        expect(res).to include("não pode ficar em branco")
+      end
+
+      it "false when 'company_name' is empty" do
+        transport_company = TransportCompany.new(company_name: '')
+        transport_company.valid?
+
+        res = transport_company.errors[:company_name]
+
+        expect(res).to include("não pode ficar em branco")
+      end
+
+      it "false when 'domain' is empty" do
+        transport_company = TransportCompany.new(domain: '')
+        transport_company.valid?
+
+        res = transport_company.errors[:domain]
+
+        expect(res).to include("não pode ficar em branco")
+      end
+
+      it "false when 'registration_number' is empty" do
+        transport_company = TransportCompany.new(registration_number: '')
+        transport_company.valid?
+
+        res = transport_company.errors[:registration_number]
+
+        expect(res).to include("não pode ficar em branco")
+      end
+
+      it "false when 'full_address' is empty" do
+        transport_company = TransportCompany.new(full_address: '')
+        transport_company.valid?
+
+        res = transport_company.errors[:full_address]
+
+        expect(res).to include("não pode ficar em branco")
+      end
+    end
+
+    context 'uniqueness' do
+      it "false when 'trading_name' is not unique" do
+        first_transport_company =  TransportCompany.create!(trading_name: "SEDEX", company_name: "SEDEX DISTRIBUICOES LTDA", domain: "sedex.com.br", registration_number: "34028316000103", full_address: "Rua dos Andares, 294")
+
+        second_transport_company = TransportCompany.new(trading_name: "SEDEX")
+
+        second_transport_company.valid?
+
+        res = second_transport_company.errors[:trading_name]
+
+        expect(res).to include("já está em uso")
+      end
+
+      it "false when 'company_name' is not unique" do
+        first_transport_company =  TransportCompany.create!(trading_name: "SEDEX", company_name: "SEDEX DISTRIBUICOES LTDA", domain: "sedex.com.br", registration_number: "34028316000103", full_address: "Rua dos Andares, 294")
+
+        second_transport_company = TransportCompany.new(company_name: "SEDEX DISTRIBUICOES LTDA")
+
+        second_transport_company.valid?
+
+        res = second_transport_company.errors[:company_name]
+
+        expect(res).to include("já está em uso")
+      end
+
+      it "false when 'domain' is not unique" do
+        first_transport_company =  TransportCompany.create!(trading_name: "SEDEX", company_name: "SEDEX DISTRIBUICOES LTDA", domain: "sedex.com.br", registration_number: "34028316000103", full_address: "Rua dos Andares, 294")
+
+        second_transport_company = TransportCompany.new(domain: "sedex.com.br")
+
+        second_transport_company.valid?
+
+        res = second_transport_company.errors[:domain]
+        expect(res).to include("já está em uso")
+      end
+
+      it "false when 'registration_number' is not unique" do
+        first_transport_company =  TransportCompany.create!(trading_name: "SEDEX", company_name: "SEDEX DISTRIBUICOES LTDA", domain: "sedex.com.br", registration_number: "34028316000103", full_address: "Rua dos Andares, 294")
+
+        second_transport_company = TransportCompany.new(registration_number: "34028316000103")
+
+        second_transport_company.valid?
+
+        res = second_transport_company.errors[:registration_number]
+        expect(res).to include("já está em uso")
+      end
+
+    end
+
+    context "format" do
+      it "false when 'registration_number' is in invalid format" do
+        transport_company = TransportCompany.new(registration_number: '234329432942934324')
+        transport_company.valid?
+
+        res = transport_company.errors[:registration_number]
+
+        expect(res).to include("não é válido")
+      end
+
+      it "false when 'domain' length is above the required" do
+        transport_company = TransportCompany.new(domain: 'sedexasndjsanddnsajndsajdnsajdjasdjsandjsa.com.br')
+        transport_company.valid?
+
+        res = transport_company.errors[:domain]
+        expect(res).to include("é muito longo (máximo: 30 caracteres)")
+      end
+    end
+
+  end
 end
