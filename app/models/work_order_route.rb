5309ab8f2ba5bac7e35f493f85d7c6db5594e27d
murichristopher <murichristopher@outlook.com>
2022-05-31 18:26:24 -0300

    Não permite que datas sejam registradas no futuro

--- app/models/work_order_route.rb
+++ app/models/work_order_route.rb
@@ -5,11 +5,18 @@ class WorkOrderRoute < ApplicationRecord
   before_validation :get_carrier_vehicle
 
   validates :title, length: {maximum: 50}
-  validates :carrier_vehicle, :title, :last_location, presence: true
+  validates :carrier_vehicle, :title, :last_location, :date, presence: true
   has_one :carrier_vehicle
+  validate :expiration_date_cannot_be_in_the_future
 
 
   private
+
+  def expiration_date_cannot_be_in_the_future
+    if date.present? && date > Date.today
+      errors.add(:date, "não pode ser futura")
+    end
+  end
 
   def calc_elapsed_time
     self.elapsed_time = (Date.today - date).to_i
