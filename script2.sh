066cb77bd5066bfe1c18ee10efd0c39ef67cda3c
murichristopher <murichristopher@outlook.com>
2022-12-04 20:50:02 -0300

    coco

--- script2.sh
+++ script2.sh
@@ -1,2 +1,2 @@
-git diff main... --name-only
-find $(git diff main... --name-only) -name "*.html.erb" -type f -print0 | xargs -0 bundle exec htmlbeautifier
+git diff origin/main... --name-only
+find $(git diff origin/main... --name-only) -name "*.html.erb" -type f -print0 | xargs -0 bundle exec htmlbeautifier
