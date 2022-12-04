473dcb4d3589ec5d74318b4f8571cbb01a1e5af4
muri <99483672+murichristopher@users.noreply.github.com>
2022-12-04 17:57:46 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+s
 admin = Admin.create!(email: "admin@admin.com", password:"123456")
 User.create!(email: "joao@visitante.com", password:"123456")
 
