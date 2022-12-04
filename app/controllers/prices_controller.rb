1842415bc0cf9cd62e6cfeee44cbb12295eed36c
murichristopher <murichristopher@outlook.com>
2022-12-04 20:55:28 -0300

    black

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -10,18 +10,18 @@ class PricesController < ApplicationController
     @price = Price.new
   end
 
-  def create
-    @price = Price.new(price_params)
+                                                                                        def create
+                                                                                          @price = Price.new(price_params)
 
-    @price.transport_company = current_user.transport_company
+                                                                                          @price.transport_company = current_user.transport_company
 
-    if @price.save
-      redirect_to(prices_path, notice:"Preço cadastrado com sucesso!")
-    else
-      @errors = @price.errors.full_messages
-      render :new
-    end
-  end
+                                                                                          if @price.save
+                                                                                            redirect_to(prices_path, notice:"Preço cadastrado com sucesso!")
+                                                                                          else
+                                                                                            @errors = @price.errors.full_messages
+                                                                                            render :new
+                                                                                          end
+                                                                                        end
 
   def edit
   end
